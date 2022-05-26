c4febc2c1aa5cb906c0f3a56c0a0e180110c2d22
ej11240 <hwangej1124@gmail.com>
2022-05-26 12:59:44 +0900

    폴더 구조 변경

--- src/com/company/바이러스.java
+++ src/com/company/바이러스.java
@@ -1,9 +1,8 @@
-package DFS;
+package com.company;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
 
 public class 바이러스 {
     static int[][] graph;
